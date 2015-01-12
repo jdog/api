333b3cf91b926e6146bcbff5dd8ae3251fbfe9ce
justinkempton <justin@kempton.com>
2015-01-11 17:44:19 -0800

    fix loading for remote repos

--- page.modules.remoteAPI.js
+++ page.modules.remoteAPI.js
@@ -3,20 +3,25 @@ PAGE.add("Modules.remoteAPI", (function() {
 	var dog = {
 	}
 
-	PAGE.loadScript(
-		"//jdog.github.io/api/page.extend.batchCallback.js"
-		, "//jdog.github.io/api/page.ajax.js"
-		, "//jdog.github.io/api/page.Constructors.APIMethod.js"
-		, "//jdog.github.io/api/page.Constructors.APIIndex.js"
-		, "//jdog.github.io/api/page.functions.createLegend.js"
-		, "//jdog.github.io/api/page.ColorizeMap.javascript.js"
-		, "//jdog.github.io/api/page.ColorizeMap.jDog.js"
-		, "//jdog.github.io/api/page.ColorizeMap.generic.js"
-		, "//jdog.github.io/api/page.Constructors.ColorizeCode.js"
-		, "//jdog.github.io/api/page.clone.js"
-		, "//jdog.github.io/api/page.modules.dom.js"
-		, "//jdog.github.io/api/page.Modules.navigation.js"
-		, true)
+	// ensures that base has fully loaded
+	PAGE.wait("clone", function() {
+
+		PAGE.loadScript(
+			"//jdog.github.io/api/page.extend.batchCallback.js"
+			, "//jdog.github.io/api/page.ajax.js"
+			, "//jdog.github.io/api/page.Constructors.APIMethod.js"
+			, "//jdog.github.io/api/page.Constructors.APIIndex.js"
+			, "//jdog.github.io/api/page.functions.createLegend.js"
+			, "//jdog.github.io/api/page.ColorizeMap.javascript.js"
+			, "//jdog.github.io/api/page.ColorizeMap.jDog.js"
+			, "//jdog.github.io/api/page.ColorizeMap.generic.js"
+			, "//jdog.github.io/api/page.Constructors.ColorizeCode.js"
+			, "//jdog.github.io/api/page.clone.js"
+			, "//jdog.github.io/api/page.modules.dom.js"
+			, "//jdog.github.io/api/page.Modules.navigation.js"
+			, true)
+
+	})
 
 	return dog
 
