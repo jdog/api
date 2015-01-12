580767161caa291df0d79597a6367d95b41bcc9a
justinkempton <justin@kempton.com>
2015-01-11 20:57:42 -0800

    adding more stuff to the remote code so local level has less to do

--- page.modules.remoteAPI.js
+++ page.modules.remoteAPI.js
@@ -20,6 +20,115 @@ PAGE.add("Modules.remoteAPI", (function() {
 		, "//jdog.github.io/api/page.Modules.navigation.js"
 		, true)
 
-	return true
+	var f = new Function()
+	var dog = {
+		data : {}
+		, methods : []
+		, sections : []
+		, e_root : document.querySelector("#API")
+		, e_legend : document.querySelector("#APILegend")
+		, legend : { }
+		, batch : undefined
+		, apiIndex : undefined
+
+		, build : f
+		, createSection : f
+		, legend : f
+		, clickCurrent : f
+		, buildAllSections : f
+	}
+	, J = jDog
+
+
+	// builds out by the an array
+	var build = dog.build = function build(e_root, methods) {
+		for (var x in methods) {
+			// adds it to the methods in whatever order it comes in careful
+			dog.methods.push(
+				ref.APIMethod(e_root, methods[x])
+			)
+		}
+
+	}
+
+	var createSection = dog.createSection = function createSection(path) {
+
+		var e_section = document.createElement("div")
+			, section = []
+
+		// adds it in the order it comes
+		dog.sections.push(section)
+
+		dog.e_root.appendChild(e_section)
+
+		PAGE.wait(path, function(data) {
+
+			for (var x in data.Methods) section.push( data.Methods[x] )
+			build(e_section, data.Methods)
+			dog.batch.tryFinish()
+		})
+	}
+
+	var legend = dog.legend = function legend(callback) {
+		PAGE.wait(
+			"Functions.createLegend"
+			, "Constructors.APIIndex"
+			, ref
+			, function() {
+
+				ref.createLegend(ref.dom, dog)
+				dog.apiIndex = ref.APIIndex(dog.e_legend, dog.methods, dog.e_root)
+
+				;(callback || f)()
+
+			})
+	}
+
+	// ensures that the element exists before clicking
+	var clickCurrent = dog.clickCurrent = function clickCurrent(hash) {
+		var e_currentLink = dog.e_legend.querySelector("a[href='" + hash + "']")
+		, e_currentMethod = document.getElementById(hash.substr(1))
+
+		if (e_currentLink && e_currentMethod)
+			e_currentLink.dispatchEvent(new Event("click"))
+		else
+			setTimeout(function() {
+				clickCurrent(hash)
+			}, 500)
+	}
+
+	var buildAllSections = dog.buildAllSections = function buildAllSections(arr) {
+		dog.batch = new ref.BatchCallback(arr.length, function() {
+
+			legend(function() {
+
+				if (location.hash)
+					clickCurrent(location.hash)
+				else
+					dog.e_legend.querySelector("a").dispatchEvent(new Event("fakeClick"))
+
+			})
+
+		})
+
+		// page should now load progressively
+		// built this way to improve perceived speed
+		for (var x in arr) createSection( arr[x] )
+	}
+
+	function init() {
+		// do nothing, should happen at local level
+	}
+
+	PAGE.wait(
+		"ajax"
+		, "Constructors.APIMethod" 
+		, "BatchCallback" 
+		, "Modules.dom"
+		, "ready" // dom is loaded
+		, ref
+		, init)
+
+	return dog
 
 }()))
